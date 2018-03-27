e5d1979596ee56221966c80e3a68861658c75aea
iosliutingxin <17787148484@qq.com>
2018-03-27 18:37:28 +0800

    抽屉效果

---
+++ 网易/Classes/View/menuView.swift
@@ -0,0 +1,21 @@
+//
+//  menuView.swift
+//  网易
+//
+//  Created by 李孔文 on 2018/3/27.
+//  Copyright © 2018年 李孔文. All rights reserved.
+//
+
+import UIKit
+
+class menuView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
